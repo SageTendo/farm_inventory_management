f58d9211ae1c125ca7956a40f7e69d65b470939e
Nyasha Zishiri <23294558@sun.ac.za>
2025-04-11 11:59:22 +0200

    Model updates, fixes and ralations implementation

--- client/src/db/schema.ts
+++ client/src/db/schema.ts
@@ -1,5 +1,5 @@
-import { sql } from 'drizzle-orm';
-import { sqliteTable, text, integer, check, real } from 'drizzle-orm/sqlite-core';
+import { relations, sql } from 'drizzle-orm';
+import { sqliteTable, text, integer, check, real, index } from 'drizzle-orm/sqlite-core';
 
 // *******************************************
 // How to create db and perform migrations
@@ -7,27 +7,33 @@ import { sqliteTable, text, integer, check, real } from 'drizzle-orm/sqlite-core
 // npx drizzle-kit generate (migrations)
 // *******************************************
 
+// User Role Types Enum
+const roleTypes = ['ADMIN', 'OWNER', 'STAFF'] as const;
 
+// *******************************************
+// Models
+// *******************************************
 // User Model
-// TODO: one to many on sale
-// TODO: one to many on exchange rate
-// TODO: many to many to stock
-// TODO: many to one on role
 export const user = sqliteTable('user', {
   id: integer().primaryKey(),
   fullname: text({ length: 100 }).notNull(),
-  username: text({ length: 50 }).notNull().unique(),
+  username: text({ length: 50 })
+    .notNull()
+    .unique(),
   passwordHash: text({ length: 256 }).notNull(),
   roleID: integer()
-    .notNull()
-    .references(() => role.id, { onDelete: 'set null' }),
-  createdAt: integer({ mode: 'timestamp' }).default(sql`CURRENT_TIMESTAMP`).notNull(),
-  updatedAt: integer({ mode: 'timestamp' }).default(sql`CURRENT_TIMESTAMP`).notNull(),
-})
+    .references(() => role.id, { onDelete: 'set null' })
+    .notNull(),
+  createdAt: integer({ mode: 'timestamp' })
+    .default(sql`CURRENT_TIMESTAMP`)
+    .notNull(),
+  updatedAt: integer({ mode: 'timestamp' })
+    .default(sql`CURRENT_TIMESTAMP`)
+    .notNull(),
+  isActive: integer({ mode: 'boolean' }).notNull()
+});
 
 // User Role Model
-// TODO: one to many to users
-export const roleTypes = ['ADMIN', 'OWNER', 'STAFF'] as const;
 export const role = sqliteTable('role', {
   id: integer().primaryKey(),
   type: text({ enum: roleTypes })
@@ -37,65 +43,127 @@ export const role = sqliteTable('role', {
   (table) => [
     check("type_check", sql`${table.type} IN ('ADMIN', 'OWNER', 'STAFF')`)
   ]
-)
+);
 
 // Exchange Rate Model
-// TODO: many to one on users
-// TODO: one to many sale
 export const exchangeRate = sqliteTable('exchange_rate', {
   id: integer().primaryKey(),
-  userID: integer(),
-  rate: real().notNull(), // round to 4 decimals
-  updatedAt: integer({ mode: 'timestamp' }).default(sql`CURRENT_TIMESTAMP`).notNull(),
-})
+  userID: integer()
+    .references(() => user.id, { onDelete: 'set null' })
+    .notNull(),
+  rate: real().notNull(), // TODO: round to 4 decimals in crud operations
+  updatedAt: integer({ mode: 'timestamp' })
+    .default(sql`CURRENT_TIMESTAMP`)
+    .notNull(),
+});
 
 // Stock Model
-// TODO: many to many for users
+// NB: This should probaly be deleted manually when a product is marked as deleted?
 export const stock = sqliteTable('stock', {
   id: integer().primaryKey(),
-  userID: integer().references(() => user.id, { onDelete: 'set null' }),
-  productID: integer('product_id').references(() => stock.id, { onDelete: 'cascade' }), //review onDelete
+  userID: integer()
+    .references(() => user.id, { onDelete: 'set null' })
+    .notNull(),
+  productID: integer('product_id')
+    .references(() => product.id, { onDelete: 'cascade' })
+    .notNull(),
   quantity: integer().notNull(),
-  lowStockThreshold: integer().$default(() => 10).notNull()
-})
+  lowStockThreshold: integer()
+    .$default(() => 10)
+    .notNull()
+});
 
 // Product Model
-// TODO: Add reference to who added the product
-// TODO: one to many on sale items
 export const product = sqliteTable('product', {
   id: integer().primaryKey(),
   name: text({ length: 100 }).notNull(),
-  buyPrice: real().notNull(), // round to 2 decimals
-  sellPrice: real().notNull(), // round to 2 decimals
-  createdAt: integer({ mode: 'timestamp' }).default(sql`CURRENT_TIMESTAMP`).notNull(),
-})
+  buyPrice: real().notNull(), // TODO: round to 2 decimals in crud operations
+  sellPrice: real().notNull(), // TODO: round to 2 decimals in crud operations
+  addedBy: integer()
+    .references(() => user.id, { onDelete: 'set null' })
+    .notNull(),
+  isDeleted: integer({ mode: 'boolean' }).default(false),
+  createdAt: integer({ mode: 'timestamp' })
+    .default(sql`CURRENT_TIMESTAMP`)
+    .notNull(),
+},
+  (table) => [
+    index('name_idx').on(table.name)
+  ]
+);
 
 // Sale Model
-// TODO: many to one on users
-// TODO: many to one on exchange rate
-// TODO: one to many on sale items
 export const sale = sqliteTable('sale', {
   id: integer().primaryKey(),
-  sellerId: integer().references(() => user.id, { onDelete: 'set null' }),
-  exchangeRate: real().references(() => exchangeRate.id, { onDelete: 'restrict' }),
+  sellerId: integer()
+    .references(() => user.id, { onDelete: 'set null' })
+    .notNull(),
+  exchangeRateId: integer()
+    .references(() => exchangeRate.id, { onDelete: 'restrict' })
+    .notNull(),
   usedLocalCurrency: integer({ mode: 'boolean' }).notNull(),
-  totalAmount: real().notNull(),
-  amountPaid: real().notNull(),
-  changeReceived: real().notNull(),
-  createdAt: integer({ mode: 'timestamp' }).default(sql`CURRENT_TIMESTAMP`).notNull()
+  totalAmount: real().notNull(), // TODO: round to 2 decimals in crud operations
+  amountPaid: real().notNull(), // TODO: round to 2 decimals in crud operations
+  changeReceived: real().notNull(), // TODO: round to 2 decimals in crud operations
+  createdAt: integer({ mode: 'timestamp' })
+    .default(sql`CURRENT_TIMESTAMP`)
+    .notNull()
 })
 
 // Sale Item Model
-// TODO: many to one on sale
-// TODO: many to one on products
 export const saleItem = sqliteTable('sale_item', {
   id: integer().primaryKey(),
-  // saleId: integer().references(() => sale.id, { onDelete: 'cascade' }),
-  productId: integer().references(() => product.id, { onDelete: 'restrict' }),
+  saleId: integer()
+    .references(() => sale.id, { onDelete: 'cascade' })
+    .notNull(),
+  productId: integer()
+    .references(() => product.id, { onDelete: 'restrict' })
+    .notNull(),
   quantity: integer().notNull(),
   unitPrice: real().notNull(),
   createdAt: integer({ mode: 'timestamp' }).notNull()
 })
 
+// *******************************************
+// Relationships
+// *******************************************
+export const userStock = sqliteTable('user_stock', { // Join table on user and stocks
+  userId: integer()
+    .references(() => user.id, { onDelete: 'set null' })
+    .notNull(),
+  stockId: integer()
+    .references(() => stock.id, { onDelete: 'cascade' })
+    .notNull(),
+  quantity: integer().notNull()
+});
 
+export const userRelations = relations(user, ({ one, many }) => ({
+  role: one(role, {
+    fields: [user.roleID],
+    references: [role.id]
+  }),
 
+  sales: many(sale),
+  stocksUpdates: many(userStock)
+}));
+
+export const roleRelations = relations(role, ({ many }) => ({
+  users: many(user)
+}));
+
+export const saleRelations = relations(sale, ({ one }) => ({
+  seller: one(user, {
+    fields: [sale.sellerId],
+    references: [user.id]
+  }),
+}));
+
+// export const productRelations = null
+// export const stockRelations = relations(stock, ({ one, many }) => {
+// })
+// export const exchangeRateRelations = null
+// export const saleItemRelations = null
+
+// TODO: One to Many Sale and Sale Item Relations
+// TODO: One to Many Product and Sale Item Relations
+// TODO: Many to Many User and Stock Relations
