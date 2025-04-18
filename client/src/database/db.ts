5968c596c966107e07fd0ffeac62c76f77233e67
Nyasha Zishiri <23294558@sun.ac.za>
2025-04-18 04:29:20 +0200

    Database setup scripts implementation

--- client/src/database/db.ts
+++ client/src/database/db.ts
@@ -2,7 +2,6 @@ import { drizzle } from "drizzle-orm/better-sqlite3";
 
 export const db = drizzle(process.env.DATABASE_URL);
 
-console.log("Creating and configuring application database...");
 db.run("PRAGMA journal_mode = WAL;");
 db.run("PRAGMA foreign_keys = ON;");
-console.log("Database creationg and configuration complete...");
+console.log("Database creation and configuration complete...");
