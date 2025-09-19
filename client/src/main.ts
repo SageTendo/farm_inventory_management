1b24e93032d7a3901b9059286be5e5c9d56571ed
SageTendo <39864316+SageTendo@users.noreply.github.com>
2025-09-19 15:42:02 +0200

    feat: DB setup and initial data population

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,12 +1,25 @@
 import { app, BrowserWindow } from "electron";
 import path from "node:path";
 import started from "electron-squirrel-startup";
-import { getDb } from "./main/database/db";
+import { ServiceRegistry } from "./main/service";
+import { env } from "./config";
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (started) {
   app.quit();
 }
+
+const populateInitialData = () => {
+  const roleService = ServiceRegistry.getInstance().getService("roleService");
+  const userService = ServiceRegistry.getInstance().getService("userService");
+
+  roleService.populateDefaultRoles();
+  userService.populateDefaultAdmin(
+    "Administrator",
+    env.DB_ADMIN_USERNAME,
+    env.DB_ADMIN_PASSWORD
+  );
+};
 
 const createWindow = () => {
   // Create the browser window.
@@ -34,7 +47,10 @@ const createWindow = () => {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on("ready", createWindow);
+app.on("ready", () => {
+  createWindow();
+  populateInitialData();
+});
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
