d1700a63c75ccebc4f02418258006331bc7b26fb
SageTendo <39864316+SageTendo@users.noreply.github.com>
2025-11-02 22:06:06 +0200

    Fix: checkout screen modal not closing on mobile Fix: floating cart button not showing on mobile Refactor: move states from props to Jotai atomsFix

---
+++ client/src/renderer/atoms/shop.atom.ts
@@ -0,0 +1,4 @@
+import { atom } from "jotai";
+
+export const cartOpenAtom = atom(false);
+export const checkoutScreenOpenAtom = atom(false);
--- client/src/renderer/ui/components/pos/cart/Main.tsx
+++ client/src/renderer/ui/components/pos/cart/Main.tsx
@@ -2,20 +2,22 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
 import { CartItemDTO } from "../../../../../shared/dto/product";
 import { Cart } from "./Cart";
-import { useState } from "react";
+import { useAtom, useAtomValue } from "jotai";
+import {
+  cartOpenAtom,
+  checkoutScreenOpenAtom,
+} from "../../../../atoms/shop.atom";
+import { useDetectScreenType } from "../../../../hooks/useDetectScreenType";
+import { useEffect } from "react";
 
-// TODO: Move states from props
 interface CartProps {
   cart: CartItemDTO[];
   cartItemsCount: number;
   cartTotalUSD: string;
   cartTotalZIG: string;
-  isCheckoutScreenOpen: boolean;
-  isMobile: boolean;
   onChangeQuantity: (id: string, delta: number) => void;
   onRemoveItem: (id: string) => void;
   clearCart: () => void;
-  onClose?: () => void; // Optional, used only in mobile modal
   onCheckout: () => void;
 }
 
@@ -23,16 +25,22 @@ export const CartPanel = ({
   cart,
   cartTotalUSD,
   cartTotalZIG,
-  isMobile,
-  isCheckoutScreenOpen,
   onChangeQuantity,
   onRemoveItem,
   cartItemsCount,
   clearCart,
-  onClose,
   onCheckout,
 }: CartProps) => {
-  const [isCartOpen, setIsCartOpen] = useState(false);
+  const isMobile = useDetectScreenType();
+  const [isCartOpen, setIsCartOpen] = useAtom(cartOpenAtom);
+  const isCheckoutScreenOpen = useAtomValue(checkoutScreenOpenAtom);
+
+  useEffect(() => {
+    if (cartItemsCount === 0) {
+      setIsCartOpen(false);
+    }
+  }, [cartItemsCount]);
+
   return (
     <>
       {!isMobile && cart.length > 0 ? (
@@ -46,39 +54,40 @@ export const CartPanel = ({
             removeItem={onRemoveItem}
             clearCart={clearCart}
             onCheckout={onCheckout}
-            onClose={onClose}
           />
         </div>
       ) : (
-        (isMobile &&
-          !isCartOpen &&
-          cart.length > 0 &&
-          !isCheckoutScreenOpen && (
-            <button
-              onClick={() => setIsCartOpen(true)}
-              className="fixed bottom-4 right-4 bg-green-600 hover:bg-green-700 text-white rounded-full w-16 h-16 flex items-center justify-center shadow-lg z-50"
-            >
-              <FontAwesomeIcon icon={faCartShopping} size="xl" />
-              <span className="absolute top-0 right-0 -mt-3 -mr-1 bg-red-600 text-white text-xs font-bold rounded-full w-6 h-6 flex items-center justify-center">
-                {cart.length}
-              </span>
-            </button>
-          ),
-        isMobile && isCartOpen && cart.length > 0 && (
-          <div className="fixed inset-0 z-50 md:z-50 bg-gray-900 text-white flex flex-col px-3 py-2">
-            <Cart
-              cart={cart}
-              cartItemsCount={cartItemsCount}
-              cartTotalUSD={cartTotalUSD}
-              cartTotalZIG={cartTotalZIG}
-              changeQuantity={onChangeQuantity}
-              removeItem={onRemoveItem}
-              clearCart={clearCart}
-              onClose={onClose}
-              onCheckout={onCheckout}
-            />
-          </div>
-        ))
+        <>
+          {isMobile &&
+            !isCartOpen &&
+            cart.length > 0 &&
+            !isCheckoutScreenOpen && (
+              <button
+                onClick={() => setIsCartOpen(true)}
+                className="fixed bottom-8 right-4 bg-green-600 hover:bg-green-700 text-white rounded-full w-16 h-16 flex items-center justify-center shadow-lg z-50"
+              >
+                <FontAwesomeIcon icon={faCartShopping} size="xl" />
+                <span className="absolute top-0 right-0 -mt-3 -mr-1 bg-red-600 text-white text-xs font-bold rounded-full w-6 h-6 flex items-center justify-center">
+                  {cart.length}
+                </span>
+              </button>
+            )}
+
+          {isMobile && isCartOpen && cart.length > 0 && (
+            <div className="fixed inset-0 z-50 md:z-50 bg-gray-900 text-white flex flex-col px-3 py-2">
+              <Cart
+                cart={cart}
+                cartItemsCount={cartItemsCount}
+                cartTotalUSD={cartTotalUSD}
+                cartTotalZIG={cartTotalZIG}
+                changeQuantity={onChangeQuantity}
+                removeItem={onRemoveItem}
+                clearCart={clearCart}
+                onCheckout={onCheckout}
+              />
+            </div>
+          )}
+        </>
       )}
     </>
   );
